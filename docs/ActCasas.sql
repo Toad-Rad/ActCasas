6f645ce5b38b65926591b3af71a638c7893e5235
alex <ramirezpedrozaalexei@gmail.com>
2023-06-10 22:41:32 -0600

    [feat]Agrego la base de datos y documento para guía

--- docs/ActCasas.sql
+++ docs/ActCasas.sql
@@ -16,6 +16,84 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `ajolotes`
+--
+
+DROP TABLE IF EXISTS `ajolotes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ajolotes` (
+  `ID_CASA` int(11) NOT NULL,
+  `Ptotales` int(11) DEFAULT NULL,
+  `Puntos_agregados` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_CASA`),
+  KEY `Puntos_agregados` (`Puntos_agregados`),
+  CONSTRAINT `ajolotes_ibfk_1` FOREIGN KEY (`Puntos_agregados`) REFERENCES `usuarios` (`ID_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ajolotes`
+--
+
+LOCK TABLES `ajolotes` WRITE;
+/*!40000 ALTER TABLE `ajolotes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ajolotes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `halcones`
+--
+
+DROP TABLE IF EXISTS `halcones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `halcones` (
+  `ID_CASA` int(11) NOT NULL,
+  `Ptotales` int(11) DEFAULT NULL,
+  `Puntos_agregados` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_CASA`),
+  KEY `Puntos_agregados` (`Puntos_agregados`),
+  CONSTRAINT `halcones_ibfk_1` FOREIGN KEY (`Puntos_agregados`) REFERENCES `usuarios` (`ID_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `halcones`
+--
+
+LOCK TABLES `halcones` WRITE;
+/*!40000 ALTER TABLE `halcones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `halcones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teporingos`
+--
+
+DROP TABLE IF EXISTS `teporingos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teporingos` (
+  `ID_CASA` int(11) NOT NULL,
+  `Ptotales` int(11) DEFAULT NULL,
+  `Puntos_agregados` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_CASA`),
+  KEY `Puntos_agregados` (`Puntos_agregados`),
+  CONSTRAINT `teporingos_ibfk_1` FOREIGN KEY (`Puntos_agregados`) REFERENCES `usuarios` (`ID_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teporingos`
+--
+
+LOCK TABLES `teporingos` WRITE;
+/*!40000 ALTER TABLE `teporingos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teporingos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuarios`
 --
 
@@ -27,7 +105,6 @@ CREATE TABLE `usuarios` (
   `Nombre` varchar(50) DEFAULT NULL,
   `Pass` varchar(50) DEFAULT NULL,
   `Casa` int(11) DEFAULT NULL,
-  `Puntos` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID_usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -38,7 +115,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Alexei','1234567',0,5);
+INSERT INTO `usuarios` VALUES (1,'Alexei','1234567',0);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-10 21:14:30
+-- Dump completed on 2023-06-10 22:34:37
